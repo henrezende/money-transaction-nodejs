67d9c902f3fe1ae06ec280f1525a6e5bd560776d
Henrique Rezende <henrique.64@hotmail.com>
2023-10-21 21:04:13 -0300

    wip: deposit and withdraw routes init

--- producer/src/routes.js
+++ producer/src/routes.js
@@ -23,4 +23,40 @@ routes.post("/transfer", async (req, res) => {
   return res.json({ ok: true });
 });
 
+routes.post("/deposit", async (req, res) => {
+  // await req.producer.connect();
+
+  console.log("deposit22--------------------------------------");
+
+  // await req.producer.send({
+  //   topic: process.env.TOPIC,
+  //   compression: CompressionTypes.GZIP,
+  //   messages: [{ value: JSON.stringify(message) }],
+  // });
+
+  // await req.producer.disconnect();
+
+  return res.json({ ok: true });
+});
+
+routes.post("/withdraw", async (req, res) => {
+  // await req.producer.connect();
+  // const message = {
+  //   user: { id: 1, name: "teste" },
+  //   grade: 10,
+  // };
+
+  console.log("withdraw22--------------------------------------");
+
+  // await req.producer.send({
+  //   topic: process.env.TOPIC,
+  //   compression: CompressionTypes.GZIP,
+  //   messages: [{ value: JSON.stringify(message) }],
+  // });
+
+  // await req.producer.disconnect();
+
+  return res.json({ ok: true });
+});
+
 export default routes;
